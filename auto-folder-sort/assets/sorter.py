06cac3fc933e6229e2a375c4257200e2c8cd0a20
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-02 20:24:28 +0000

    Added update_dir_files function and reformatted conditional statements

--- auto-folder-sort/assets/sorter.py
+++ auto-folder-sort/assets/sorter.py
@@ -34,8 +34,15 @@ class Sorter:
         self.sort_type = sort_type
         self.earliest_year = earliest_year
 
-        # Used in self.sort() to call the correct sort function based on sort type
-        self.s_dict = {"file_type": self.sort_file, "date": self.sort_date}
+        # Used in self.sort() to call the correct sort function based
+        # on sort type
+        self.s_dict: dict = {
+            "file_type": self.sort_file,
+            "date": self.sort_date
+        }
+
+        # List of all files/folders present in self.folder
+        self.dir_files: list = os.listdir(self.folder)
 
     def assert_valid(self):
         """Returns whether the provided constructor arguments are valid.
@@ -64,38 +71,55 @@ class Sorter:
         >>> sorter = Sorter("./", 'date')
         >>> sorter.assert_valid()
         False
-
-        >>> sorter = Sorter(__file__, 'file_type')
-        >>> sorter.assert_valid()
-        False
         """
         self.today = datetime.today()
         self.year = self.today.year
 
         is_valid_folder = (
-            type(self.folder) == str
-            and os.path.isdir(self.folder)
-            and os.path.isabs(self.folder)
+            type(self.folder) == str and
+            os.path.isdir(self.folder) and
+            os.path.isabs(self.folder)
         )
 
         is_valid_sort = self.sort_type in ["date", "file_type"]
 
         is_valid_earliest = (
-            type(self.earliest_year) == int and 1920 <= self.earliest_year <= self.year
+            type(self.earliest_year) == int and
+            1920 <= self.earliest_year <= self.year
         )
 
         return is_valid_folder and is_valid_sort and is_valid_earliest
 
-    def check_file_folders(self) -> None:
+    def update_dir_files(self) -> None:
+        """Updates the list of files/folders present in self.folder.
+
+        Examples:
+        >>> sorter = Sorter(os.path.dirname(__file__), 'date', 2018)
+        >>> dir_1 = sorter.dir_files
+        >>> sorter.folder = os.path.dirname(os.path.dirname(__file__))
+        >>> sorter.update_dir_files()
+        >>> dir_1 == sorter.dir_files
+        False
+
+        >>> sorter = Sorter(os.path.dirname(__file__), 'file_type')
+        >>> dir_1 = sorter.dir_files
+        >>> sorter.update_dir_files()
+        >>> dir_1 == sorter.dir_files
+        True
+        """
+
+        self.dir_files: list = os.listdir(self.folder)
+
+    def ensure_file_folders(self) -> None:
         """Ensures sorting folders for file types are present in self.folder."""
 
-        dir_files: list = os.listdir(self.folder)
+        self.update_dir_files()
 
         for file_type in constants.FILE_FOLDERS:
-            if file_type not in dir_files:
+            if file_type not in self.dir_files:
                 os.makedir(os.path.join(self.folder, file_type))
 
-    def check_date_folders(self) -> None:
+    def ensure_date_folders(self) -> None:
         """Ensures sorting folders for dates are present in self.folder.
 
         Folders will be structured in the layout: year -> month1, month2...
@@ -104,24 +128,18 @@ class Sorter:
         self.earliest_year and the current year, inclusive.
         """
 
-        dir_files: list = os.listdir(self.folder)
+        self.update_dir_files()
 
         years: list = map(
             str, list(range(self.earliest_year, datetime.today().year + 1))
         )
 
         for year in years:
-            if year not in dir_files:
+            if year not in self.dir_files:
                 os.makedir(os.path.join(self.folder, year))
 
                 for month in constants.MONTHS:
                     os.makedir(os.path.join(self.folder, year, month))
-
-    def generate_file_folders(self):
-        pass
-
-    def generate_date_folders(self):
-        pass
 
     def sort_file(self):
         pass
