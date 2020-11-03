3577e32072d20b7fa532dd6c6f05c79d73650a97
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-03 00:05:19 +0000

    Finished but not yet tested sort function for file type and final sort function

--- auto-folder-sort/assets/sorter.py
+++ auto-folder-sort/assets/sorter.py
@@ -16,7 +16,8 @@ class Sorter:
 
         sort_type: Either 'date' or 'file_type'
 
-        earliest_year: Earliest year to create folders for if 'date' was given for sort_type
+        earliest_year: Earliest year to create folders for if 'date'
+                       was given for sort_type
 
         Examples:
         >>> sorter = Sorter(os.path.dirname(__file__), 'file_type', 2019)
@@ -34,11 +35,11 @@ class Sorter:
         self.sort_type = sort_type
         self.earliest_year = earliest_year
 
-        # Used in self.sort() to call the correct sort function based
-        # on sort type
+        # Used in self.sort() to call the correct functions
+        # based on sort type
         self.s_dict: dict = {
-            "file_type": self.sort_file,
-            "date": self.sort_date
+            "file_type": [self.ensure_file_folders, self.sort_file],
+            "date": [self.ensure_date_folders, self.sort_date],
         }
 
         # List of all files/folders present in self.folder
@@ -75,20 +76,19 @@ class Sorter:
         self.today = datetime.today()
         self.year = self.today.year
 
-        is_valid_folder = (
-            type(self.folder) == str and
-            os.path.isdir(self.folder) and
-            os.path.isabs(self.folder)
+        self.is_valid_folder = (
+            type(self.folder) == str
+            and os.path.isdir(self.folder)
+            and os.path.isabs(self.folder)
         )
 
-        is_valid_sort = self.sort_type in ["date", "file_type"]
+        self.is_valid_sort = self.sort_type in ["date", "file_type"]
 
-        is_valid_earliest = (
-            type(self.earliest_year) == int and
-            1920 <= self.earliest_year <= self.year
+        self.is_valid_earliest = (
+            type(self.earliest_year) == int and 1920 <= self.earliest_year <= self.year
         )
 
-        return is_valid_folder and is_valid_sort and is_valid_earliest
+        return self.is_valid_folder and self.is_valid_sort and self.is_valid_earliest
 
     def update_dir_files(self) -> None:
         """Updates the list of files/folders present in self.folder.
@@ -142,7 +142,33 @@ class Sorter:
                     os.makedir(os.path.join(self.folder, year, month))
 
     def sort_file(self):
-        pass
+        """Sorts self.folder by file type."""
+
+        self.update_dir_files()
+
+        try:
+            for item in self.dir_files:
+                # Don't resort the generated sort folders
+                if item in constants.FILE_FOLDERS:
+                    continue
+
+                extension = os.path.splitext(item)[-1]
+
+                for file_type, extensions in constants.FILE_FOLDERS.items():
+                    if extension in extensions:
+                        old_path = os.path.join(self.folder, item)
+                        new_path = os.path.join(self.folder, file_type, item)
+
+                        shutil.move(old_path, new_path)
+
+        except IOError as e:
+            print(
+                "\nThere was an error while sorting files by file type"
+                f"\n{e.args}"
+                "\nPlease check the log file for further information"
+            )
+
+        return True
 
     def sort_date(self):
         pass
@@ -150,4 +176,16 @@ class Sorter:
     def sort(self):
         """Calls appropriate sort function (file or date) based on self.sort_type"""
 
-        self.s_dict[self.sort_type]()
+        if self.assert_valid():
+            self.s_dict[self.sort_type][0]()
+            self.s_dict[self.sort_type][1]()
+        else:
+            raise IOError(
+                "\nOne of the following values given was invalid (False):"
+                f"\nFolder: {self.is_valid_folder}"
+                f"\nSort type: {self.is_valid_sort}"
+                f"\nEarliest year: {self.is_valid_earliest}"
+                "\nPlease check the log file for further information"
+            )
+
+        return True
