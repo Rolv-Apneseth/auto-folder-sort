08a0326337857ae86f5c5c7591f32f992ac3fb73
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-13 18:17:01 +0000

    Defined custom event handler class for watchdog

--- auto-folder-sort/main.py
+++ auto-folder-sort/main.py
@@ -1,4 +1,15 @@
 import os
+import sys
+import time
+import logging
 import watchdog
 
 import assets.sorter
+
+
+class CustomEventHandler(watchdog.FileSystemEventHandler):
+    def __init__(self, sorter):
+        self.sorter = sorter
+
+    def on_modified(self, event):
+        self.sort()
