87de3e461fa8386f841305176dfe6eeb0a31aeee
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-13 18:44:12 +0000

    Fixed some errors with import of watchdog

--- auto-folder-sort/main.py
+++ auto-folder-sort/main.py
@@ -1,15 +1,33 @@
+import datetime
+import logging
 import os
 import sys
 import time
-import logging
-import watchdog
 
-import assets.sorter
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from assets.sorter import Sorter
 
 
-class CustomEventHandler(watchdog.FileSystemEventHandler):
+# EVENT HANDLER
+class CustomEventHandler(FileSystemEventHandler):
     def __init__(self, sorter):
         self.sorter = sorter
 
     def on_modified(self, event):
-        self.sort()
+        self.sorter.sort()
+
+
+# HELPER FUNCTIONS
+def make_observer(folder, sort_type, earliest_year=datetime.today().year):
+    """Generates an observer object, as well as the sorter and event handler for it."""
+
+    sorter = Sorter(folder, sort_type, earliest_year)
+
+    event_handler = CustomEventHandler(sorter)
+
+    observer = Observer()
+    observer.schedule(event_handler, folder, recursive=True)
+
+    return observer
