a17431571c8681bec69e8f9ff3e72b7c350904c4
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-14 17:10:38 +0000

    Designed initial layout of the Main class and completed a backup function for the pickle file which will be used

--- auto-folder-sort/main.py
+++ auto-folder-sort/main.py
@@ -1,13 +1,17 @@
-from datetime import datetime
 import logging
 import os
+import pickle
 import sys
 import time
+from datetime import datetime
 
-from watchdog.observers import Observer
+from send2trash import send2trash
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from assets.sorter import Sorter
+
+FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
 # EVENT HANDLER
@@ -19,15 +23,48 @@ class CustomEventHandler(FileSystemEventHandler):
         self.sorter.sort()
 
 
-# HELPER FUNCTIONS
-def make_observer(folder, sort_type, earliest_year=datetime.today().year):
-    """Generates an observer object, as well as the sorter and event handler for it."""
+# MAIN
+class Main:
+    def __init__(self):
+        self.PICKLE_PATH = os.path.join(FILE_PATH, "assets", "observers.pkl")
+        self.BACKUP_PATH = os.path.join(FILE_PATH, "assets", "backup_observers.pkl")
 
-    sorter = Sorter(folder, sort_type, earliest_year)
+        self.observers = []
 
-    event_handler = CustomEventHandler(sorter)
+    # HELPER FUNCTIONS
+    def pickle_exists(self) -> bool:
+        return os.path.exists(self.PICKLE_PATH)
 
-    observer = Observer()
-    observer.schedule(event_handler, folder, recursive=True)
+    def backup_exists(self) -> bool:
+        return os.path.exists(self.BACKUP_PATH)
 
-    return observer
+    def make_observer(self, folder, sort_type, earliest_year):
+        """Generates an observer object, as well as the sorter and event handler for it."""
+
+        sorter = Sorter(folder, sort_type, earliest_year)
+
+        event_handler = CustomEventHandler(sorter)
+
+        observer = Observer()
+        observer.schedule(event_handler, folder, recursive=True)
+
+        return observer
+
+    def add_observer(self, folder, sort_type, earliest_year=datetime.today().year):
+        pass
+
+    def backup(self):
+        if self.backup_exists():
+            send2trash(self.BACKUP_PATH)
+
+        if self.pickle_exists():
+            os.rename(self.PICKLE_PATH, self.BACKUP_PATH)
+
+    def load_observers(self):
+        pass
+
+    def startup(self):
+        pass
+
+    def main(self):
+        pass
