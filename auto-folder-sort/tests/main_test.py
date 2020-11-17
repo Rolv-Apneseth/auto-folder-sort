f322a05f7892c73a6c0adee81dc7399eb3ef196f
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-17 16:03:42 +0000

    Added sample file folders paths as constants and started laying out structure of the unit test

--- auto-folder-sort/tests/main_test.py
+++ auto-folder-sort/tests/main_test.py
@@ -8,3 +8,18 @@ from datetime import datetime
 # `python3 -m tests.main_test`
 # from the main directory (where main.py is)
 from main import Main, CustomEventHandler
+
+# SAMPLE FILE PATHS
+SAMPLE_PATH_1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Sample Files")
+SAMPLE_PATH_2 = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "Sample Files (2)"
+)
+
+
+## Unit tests #
+class TestMain(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
