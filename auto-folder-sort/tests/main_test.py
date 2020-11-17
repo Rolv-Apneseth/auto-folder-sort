32cb25b52418b1c9b6ea28acc11a01a4c010bd1c
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-17 15:01:51 +0000

    Set up unit test file for main.py

---
+++ auto-folder-sort/tests/main_test.py
@@ -0,0 +1,10 @@
+import logging
+import os
+import shutil
+import unittest
+from datetime import datetime
+
+# Note that to run this test, you must execute:
+# `python3 -m tests.main_test`
+# from the main directory (where main.py is)
+from main import Main, CustomEventHandler
