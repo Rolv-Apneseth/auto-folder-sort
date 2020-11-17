237f852aa3a33f761c411ff28b2eeb95e7e46f87
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-17 16:08:43 +0000

    Started tearDownClass and setUp methods

--- auto-folder-sort/tests/main_test.py
+++ auto-folder-sort/tests/main_test.py
@@ -8,6 +8,8 @@ from datetime import datetime
 # `python3 -m tests.main_test`
 # from the main directory (where main.py is)
 from main import Main, CustomEventHandler
+from tests.constants_for_tests import SAMPLE_FILES, TEST_FILE_FOLDERS
+
 
 # SAMPLE FILE PATHS
 SAMPLE_PATH_1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Sample Files")
@@ -18,7 +20,18 @@ SAMPLE_PATH_2 = os.path.join(
 
 ## Unit tests #
 class TestMain(unittest.TestCase):
-    pass
+    @classmethod
+    def tearDownClass(cls):
+        # Final assert at the end of all tests to make sure both
+        # Sample Files folders are back to their original layout
+        assert os.listdir(SAMPLE_PATH_1) == SAMPLE_FILES
+        assert os.listdir(SAMPLE_PATH_2) == SAMPLE_FILES
+
+    def setUp(self):
+
+        # Stops test from running if either folder layout is incorrect
+        assert os.listdir(SAMPLE_PATH_1) == SAMPLE_FILES
+        assert os.listdir(SAMPLE_PATH_2) == SAMPLE_FILES
 
 
 if __name__ == "__main__":
