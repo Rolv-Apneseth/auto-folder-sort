4b16db805b0884fde15a9fe2a7eeba0512afe475
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-11-17 16:36:49 +0000

    Added setUpClass method, updated tearDownClass method

--- auto-folder-sort/tests/main_test.py
+++ auto-folder-sort/tests/main_test.py
@@ -7,31 +7,44 @@ from datetime import datetime
 # Note that to run this test, you must execute:
 # `python3 -m tests.main_test`
 # from the main directory (where main.py is)
-from main import Main, CustomEventHandler
-from tests.constants_for_tests import SAMPLE_FILES, TEST_FILE_FOLDERS
+import main
+from tests.constants_for_tests import SAMPLE_FILES, TEST_FILE_FOLDERS, TESTS_DIR
 
 
-# SAMPLE FILE PATHS
-SAMPLE_PATH_1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Sample Files")
-SAMPLE_PATH_2 = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "Sample Files (2)"
-)
+# CONSTANTS
+SAMPLE_PATH_1 = os.path.join(TESTS_DIR, "Sample Files")
+SAMPLE_PATH_2 = os.path.join(TESTS_DIR, "Sample Files (2)")
+
+TEST_COMMANDS = os.path.join(TESTS_DIR, "test_folders_to_track.txt")
 
 
-## Unit tests #
+## Unit tests ##
 class TestMain(unittest.TestCase):
     @classmethod
+    def setUpClass(cls):
+        with open(TEST_COMMANDS, "w") as new_commands:
+            new_commands.write(f"{SAMPLE_PATH_1} file_type\n{SAMPLE_PATH_2} date 2018")
+
+        main.COMMANDS_PATH = TEST_COMMANDS
+
+    @classmethod
     def tearDownClass(cls):
+        os.remove(TEST_COMMANDS)
+
         # Final assert at the end of all tests to make sure both
         # Sample Files folders are back to their original layout
         assert os.listdir(SAMPLE_PATH_1) == SAMPLE_FILES
         assert os.listdir(SAMPLE_PATH_2) == SAMPLE_FILES
 
     def setUp(self):
+        self.sample_program = main.Main()
 
         # Stops test from running if either folder layout is incorrect
         assert os.listdir(SAMPLE_PATH_1) == SAMPLE_FILES
         assert os.listdir(SAMPLE_PATH_2) == SAMPLE_FILES
+
+    def tearDown(self):
+        del self.sample_program
 
 
 if __name__ == "__main__":
